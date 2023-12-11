f93dc772d5e967d75c6bdbb8bd68150b91c05bca
Joel <joeldario9912@gmail.com>
2023-12-11 14:19:07 -0300

    welcome

---
+++ src/01Welcome/welcome.go
@@ -0,0 +1,14 @@
+package welcome
+
+import (
+	"fmt"
+	"time"
+)
+
+func Hello() {
+	fmt.Println("Hello Go")
+}
+
+func Time() {
+	fmt.Println("The time is", time.Now())
+}
---
+++ src/main.go
@@ -0,0 +1,8 @@
+package main
+
+import welcome "go-tour/src/01Welcome"
+
+func main() {
+  welcome.Hello()
+	welcome.Time()
+}
